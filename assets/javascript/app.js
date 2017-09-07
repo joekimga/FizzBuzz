6a5794762cc33100c90b8312a327760192feaa7c
Joseph Kim <joekimga@gmail.com>
2017-09-07 19:48:17 -0400

    WIP

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+for (x=1; x<=100; x++) {
+	//Multiple of 3
+	if (x%3==0) {
+		alert(Fizz);
+	}
+	//Multiple of 5
+	if (x%5==0) {
+		alert(Buzz);
+	}
+	//Multiple of 3 & 5
+	if (x%3 !=0 && (x%5 !+0)){
+		alert(FizzBuzz);
+	}
+
+
+}
